166d379e6a356e6cc8da310920c63405dc146e2a
abubakerkhan9494 <112695858+abubakerkhan9494@users.noreply.github.com>
2023-06-19 22:16:18 +0530

    Add files via upload

---
+++ js/disapper.js
@@ -0,0 +1,10 @@
+function disappear(){
+	X=document.getElementById("active");
+	X.style.display="inline-block";
+	
+}
+
+function undo(){
+	X=document.getElementBId("inactive");
+	X.style.display="none";
+}
